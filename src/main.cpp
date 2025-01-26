158c4bd53aa26f70c7e3c2278774733d0fd8be67
Pang (Jeff) Liu <pangliu@brandeis.edu>
2025-01-26 14:27:30 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@ int main() {
        parser.parse();
        Graph graph(parser);
 
-       // 选择两个固定的节点作为起点和终点
-       // long long start_id = 61172421;  // 北纬: 42.4054370, 西经: 71.1258370
-       // long long end_id = 61172482;    // 北纬: 42.4040290, 西经: 71.1175103
-
         long long start_id = 1097255634;  // Tufts Garage
         long long end_id = 1479769463;  // Torbert Macdonald Park
 
